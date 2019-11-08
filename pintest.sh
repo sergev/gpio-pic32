d4c5285e93c921835d8292be9d2b12991bc104bc
Serge Vakulenko <serge.vakulenko@gmail.com>
2019-11-07 19:50:17 -0800

    Update the test script.

--- pintest.sh
+++ pintest.sh
@@ -48,47 +48,35 @@ printErrCount()
 }
 
 
-# testPins start end
+# testPin pin
 ################################################################################
 
-testPins()
+testPin()
 {
   start=$1
     end=$2
    errs=0
 
-  printf "%30s p%-2d:p%-2d: "  "$3" $1 $2
+  printf "%20s: "  "Pin p$1"
 
-# Set range to inputs
+  # Set pin to inputs
+  gpio mode p$1 in
 
-  for i in `seq $start $end`; do
-    gpio mode p$i in
-  done
+  # Enable internal pull-up and expect to read high
+  gpio mode p$1 up
+  if [ `gpio read p$1` = 0 ]; then
+    logErr p$1 1 0
+  fi
 
-# Enable internal pull-ups and expect to read high
+  # Enable internal pull-down and expect to read low
+  gpio mode p$1 down
+  if [ `gpio read p$1` = 1 ]; then
+    echo "Pin p$1 failure - expected 0, got 1"
+    let errs+=1
+  fi
 
-  for i in `seq $start $end`; do
-    gpio mode p$i up
-    if [ `gpio read p$i` = 0 ]; then
-      logErr p$i 1 0
-    fi
-  done
-
-# Enable internal pull-downs and expect to read low
-
-  for i in `seq $start $end`; do
-    gpio mode p$i down
-    if [ `gpio read p$i` = 1 ]; then
-      echo "Pin p$i failure - expected 0, got 1"
-      let errs+=1
-    fi
-  done
-
-# Remove the internal pull up/downs
-
-  for i in `seq $start $end`; do
-    gpio mode p$i tri
-  done
+  # Remove the internal pull up/down
+  gpio mode p$1 tri
 
   if [ $errs = 0 ]; then
     echo " OK"
@@ -99,35 +87,8 @@ testPins()
   let totErrs+=errs
 }
 
-
-intro()
-{
-  cat <<EOF
-PinTest
-=======
-
-This is a simple utility to test the GPIO pins on your PIC32 Starter Kit.
-
-NOTE: All GPIO peripherals must be removed to perform this test. This
-  includes serial, I2C and SPI connections. You may get incorrect results
-  if something is connected and it interferes with the test.
-
-This test can only test the input side of things. It uses the internal
-pull-up and pull-down resistors to simulate inputs. It does not test
-the output drivers.
-
-Please make sure everything is removed and press the ENTER key to continue,
-EOF
-
-  echo -n "or Control-C to abort... "
-  read a
-}
-
-
 # Start here
 ################################################################################
-
-intro
 
    errs=0
 totErrs=0
@@ -136,33 +97,53 @@ echo ""
 
 # Main pins
 
-testPins 0 0 "Pin"
-
-testPins 4 27 "The main 24 GPIO pins"
+testPin 0
+echo "              Pin p1:  Not present in hardware"
 
 # I2C - Needs somewhat different testing
 #	due to the on-board pull-up's
-
-echo -n "                  The I2C pins p2 :p3 : "
+echo -n "  The I2C pins p2-p3: "
 errs=0
 gpio mode p2 in
 gpio mode p3 in
-
 if [ `gpio read p2` = 0 ]; then
   echo "Pin p2 failure - expected 1, got 0"
   let errs+=1
 fi
-
 if [ `gpio read p3` = 0 ]; then
   echo "Pin p3 failure - expected 1, got 0"
   let errs+=1
 fi
-
 if [ $errs = 0 ]; then
   echo " OK"
 else
   printErrCount
 fi
+
+testPin 4
+testPin 5
+testPin 6
+testPin 7
+testPin 8
+testPin 9
+testPin 10
+testPin 11
+testPin 12
+testPin 13
+testPin 14
+echo "             Pin p15:  Not tested (U4RX)"
+testPin 16
+testPin 17
+testPin 18
+echo "             Pin p19:  Not tested (U2RX)"
+testPin 20
+testPin 21
+testPin 22
+testPin 23
+testPin 24
+testPin 25
+testPin 26
+testPin 27
 
 echo ""
 if [ $totErrs != 0 ]; then
