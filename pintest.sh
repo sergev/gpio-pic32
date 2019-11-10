4f22ea7440df8befef5476ff43ca973d62c1561a
Serge Vakulenko <serge.vakulenko@gmail.com>
2019-11-09 18:55:43 -0800

    Update the test script.

--- pintest.sh
+++ pintest.sh
@@ -19,83 +19,99 @@
 #
 #################################################################################
 
-
+#
 # logErr pin, expected got
-################################################################################
-
+#
 logErr ()
 {
-  if [ $errs = 0 ]; then
-    echo ""
-  fi
-  echo " --> Pin $1 failure. Expected $2, got $3"
-  let errs+=1
+    if [ $errs = 0 ]; then
+        echo ""
+    fi
+    echo " --> Pin $1 failure. Expected $2, got $3"
+    let errs+=1
 }
 
-
+#
 # printErrorCount
-################################################################################
-
+#
 printErrCount()
 {
-  if [ $errs = 0 ]; then
-    echo "No faults detected."
-  elif [ $errs = 1 ]; then
-    echo "One fault detected."
-  else
-    echo "$errs faults detected"
-  fi
+    if [ $errs = 0 ]; then
+        echo "No faults detected."
+    elif [ $errs = 1 ]; then
+        echo "One fault detected."
+    else
+        echo "$errs faults detected"
+    fi
 }
 
-
-# testPin pin
-################################################################################
-
+#
+# testPin pin [pullup]
+#
 testPin()
 {
-  start=$1
-    end=$2
-   errs=0
+    start=$1
+    pullup=$2
+    errs=0
 
-  printf "%20s: "  "Pin p$1"
+    if [ -z "$pullup" ]; then
+        printf "%20s: "  "Pin p$1"
+    else
+        printf "%20s: "  "Pin p$1 with pull-up"
+    fi
 
-  # Set pin to input
-  gpio mode p$1 in
+    # Set pin to output
+    gpio mode p$1 out
 
-  # Enable internal pull-up and expect to read high
-  gpio mode p$1 up
-  if [ `gpio read p$1` = 0 ]; then
-    logErr p$1 1 0
-  fi
+    # Set pin high and expect to read high
+    gpio write p$1 1
+    if [ `gpio read p$1` = 0 ]; then
+        logErr p$1 1 0
+    fi
 
-  # Enable internal pull-down and expect to read low
-  gpio mode p$1 down
-  if [ `gpio read p$1` = 1 ]; then
-    echo "Pin p$1 failure - expected 0, got 1"
-    let errs+=1
-  fi
+    # Set pin low and expect to read low
+    gpio write p$1 0
+    if [ `gpio read p$1` = 1 ]; then
+        echo "Pin p$1 failure - expected 0, got 1"
+        let errs+=1
+    fi
 
-  # Remove the internal pull up/down
-  gpio mode p$1 tri
+    # Set pin to input
+    gpio mode p$1 in
 
-  if [ $errs = 0 ]; then
-    echo " OK"
-  else
-    printErrCount
-  fi
+    # Enable internal pull-up and expect to read high
+    gpio mode p$1 up
+    if [ `gpio read p$1` = 0 ]; then
+        logErr p$1 1 0
+    fi
 
-  let totErrs+=errs
+    # Enable internal pull-down and expect to read low
+    if [ -z "$pullup" ]; then
+        gpio mode p$1 down
+        if [ `gpio read p$1` = 1 ]; then
+            echo "Pin p$1 failure - expected 0, got 1"
+            let errs+=1
+        fi
+    fi
+
+    # Remove the internal pull up/down
+    gpio mode p$1 tri
+
+    if [ $errs = 0 ]; then
+        echo " OK"
+    else
+        printErrCount
+    fi
+
+    let totErrs+=errs
 }
 
+#
 # Start here
-################################################################################
-
-   errs=0
+#
+errs=0
 totErrs=0
-
 echo ""
-
-# Main pins
 
 # Switch u2rx to p19 (default)
 gpio mode p19 u2rx
@@ -105,23 +121,8 @@ echo "              Pin p1:  Not present in hardware"
 
 # I2C - Needs somewhat different testing
 #	due to the on-board pull-up's
-echo -n "  The I2C pins p2-p3: "
-errs=0
-gpio mode p2 in
-gpio mode p3 in
-if [ `gpio read p2` = 0 ]; then
-  echo "Pin p2 failure - expected 1, got 0"
-  let errs+=1
-fi
-if [ `gpio read p3` = 0 ]; then
-  echo "Pin p3 failure - expected 1, got 0"
-  let errs+=1
-fi
-if [ $errs = 0 ]; then
-  echo " OK"
-else
-  printErrCount
-fi
+testPin 2 pullup
+testPin 3 pullup
 
 testPin 4
 testPin 5
@@ -140,23 +141,11 @@ testPin 17
 testPin 18
 
 # U2RX - Needs somewhat different testing
-#	 due to the on-board pull-up's
-echo -n "    The U2RX pin p19: "
-errs=0
+#	 due to the on-board pull-up
 gpio mode p3 u2rx   # Switch u2rx to p3, which also has a pull-up, so it's safe
-gpio mode p19 in
-if [ `gpio read p19` = 0 ]; then
-  echo "Pin p19 failure - expected 1, got 0"
-  let errs+=1
-fi
-if [ $errs = 0 ]; then
-  echo " OK"
-else
-  printErrCount
-fi
+testPin 19 pullup
 gpio mode p19 u2rx  # Switch u2rx back to p19
 
-#echo "             Pin p19:  Not tested (U2RX)"
 testPin 20
 testPin 21
 testPin 22
@@ -168,7 +157,7 @@ testPin 27
 
 echo ""
 if [ $totErrs != 0 ]; then
-  echo ""
-  echo "Faults detected! Output of 'readall':"
-  gpio readall
+    echo ""
+    echo "Faults detected! Output of 'readall':"
+    gpio readall
 fi
